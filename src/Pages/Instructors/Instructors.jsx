278d6d0965f4b3298927de14bd3eaf92f015e800
spkumerjoy <joyskydd@gmail.com>
2023-06-08 17:22:40 +0600

    navbar is complete

---
+++ src/Pages/Instructors/Instructors.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Instructors = () => {
+    return (
+        <div>
+            <h2>Instructors</h2>
+        </div>
+    );
+};
+
+export default Instructors;
---
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,83 @@
+import { Link, NavLink } from "react-router-dom";
+import { FaSignOutAlt, FaUserAlt } from "react-icons/fa";
+import { MdOutlineDashboard } from "react-icons/md";
+import "./NavBar.css";
+
+const NavBar = () => {
+    const navItem = (
+        <>
+            <li>
+                <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+                <NavLink to="/instructors">Instructors</NavLink>
+            </li>
+            <li>
+                <NavLink to="/classes">Classes</NavLink>
+            </li>
+        </>
+    );
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
+                            </svg>
+                        </label>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                            id="menu-item"
+                        >
+                            {navItem}
+                        </ul>
+                    </div>
+                    <Link to="/">
+                        <img src="logo.png" alt="Site logo" className="w-36" />
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul
+                        className="menu menu-horizontal px-1 text-xl  uppercase"
+                        id="menu-item"
+                    >
+                        {navItem}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <>
+                        <button className="btn glass hover:text-[#FF5300]">
+                            <MdOutlineDashboard className="text-xl" />
+                            Dashboard
+                        </button>
+                        <button className="btn text-[#FF5300] hover:text-red-600">
+                            <FaSignOutAlt className="text-xl" />
+                            Log Out
+                        </button>
+                    </>
+                    <Link>
+                        <button className="btn glass hover:text-[#FF5300]">
+                            <FaUserAlt /> Login
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
