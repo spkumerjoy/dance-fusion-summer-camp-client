acdaaa1ae200b90a337ad5cf2f35f51074a73282
spkumerjoy <joyskydd@gmail.com>
2023-06-12 12:00:55 +0600

    firebase all auth setup done

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./Routes/Routes.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./Providers/AuthProvider";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
         <React.StrictMode>
-            <RouterProvider router={router} />
+            <AuthProvider>
+                <RouterProvider router={router} />
+            </AuthProvider>
         </React.StrictMode>
     </QueryClientProvider>
 );
